f9a9567c9dbf2b6061c934a1dd5c4ccac1b5982b
SeongJun Ha <agopwns@naver.com>
2019-11-18 01:08:45 +0900

    add NowPlayingActivity

---
+++ app/src/main/java/com/example/colorplayer/NowPlayingActivity.java
@@ -0,0 +1,14 @@
+package com.example.colorplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NowPlayingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+    }
+}
