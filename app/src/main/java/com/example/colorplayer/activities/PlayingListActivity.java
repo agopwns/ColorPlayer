57ed07816b41953802a4640ddbb5fb0bc45a5729
SeongJun Ha <agopwns@naver.com>
2019-11-23 11:10:40 +0900

    add playing list

---
+++ app/src/main/java/com/example/colorplayer/activities/PlayingListActivity.java
@@ -0,0 +1,16 @@
+package com.example.colorplayer.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.colorplayer.R;
+
+public class PlayingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing_list);
+    }
+}
