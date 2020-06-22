45fd93308a6fbfff48b10493f91b7bfab57ba4aa
SeongJun Ha <agopwns@naver.com>
2020-06-22 14:52:14 +0900

    Update YoutubeApiUtils.java

--- app/src/main/java/com/example/colorplayer/youtube/YoutubeApiUtils.java
+++ app/src/main/java/com/example/colorplayer/youtube/YoutubeApiUtils.java
@@ -3,7 +3,7 @@ package com.example.colorplayer.youtube;
 public class YoutubeApiUtils {
 
 
-    public static final String API_KEY = "AIzaSyBc0My6KMhjGR4YKiB3zjqIQgdev-BFDi4";
-    // 여분키 AIzaSyAx1BPH_N4MRf_L-p80OHI87Aqq9a0Dd70
+    public static final String API_KEY = "여기다 키를 넣어주세요";
+    // 여분키 여기 키를 넣어주세요!
 
 }
